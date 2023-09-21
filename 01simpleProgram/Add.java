6e51f694b312f5d41c9a4d7e7b37e8371ae02f6f
Chinmay <chinz.sandur07@gmail.com>
2023-09-21 15:32:50 +0530

    simple program on java

---
+++ 01simpleProgram/Add.java
@@ -0,0 +1,12 @@
+class Add
+{
+	public static void main(String args[])
+	{
+		int a =20;
+		int b=30;
+		int c=a+b;
+		System.out.println("value of a is "+a);
+		System.out.println("value of b is "+b);
+		System.out.println("Addition of a and b is "+c);
+	}
+}
---
+++ 01simpleProgram/AreaOfTriangle.java
@@ -0,0 +1,10 @@
+class AreaOfTriangle
+{
+	public static void main(String args[])
+	{
+		int  base = 10;
+		int  height = 12;
+		double area = 0.5 * base * height;
+		System.out.println("Area of triangle is " + area);
+	}
+}
